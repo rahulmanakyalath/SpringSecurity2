264f3066407d0826ab7d40206bc593e2eeb87dc8
RAHUL KUMAR M S <rahulmanakyalath@gmail.com>
2024-10-09 12:49:25 +0530

    initial commit

---
+++ src/test/java/com/rkworks/SpringSecurityLearning/SpringSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkworks.SpringSecurityLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
