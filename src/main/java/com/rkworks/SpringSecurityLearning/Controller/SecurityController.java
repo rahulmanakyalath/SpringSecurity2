86acec29ef80965bd442fe3e35c47288d38c4bf1
RAHUL KUMAR M S <rahulmanakyalath@gmail.com>
2024-10-10 09:45:59 +0530

    implementing basic Auth Spirng Security with customised security filter chain and UserDetails Service

---
+++ src/main/java/com/rkworks/SpringSecurityLearning/Controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.rkworks.SpringSecurityLearning.Controller;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Security")
+public class SecurityController {
+
+
+    @GetMapping("/test")
+    public String testSecurity(HttpServletRequest request){//[since every controller is a servlet we can get the HttpServelet request ]
+
+        return "Entered the secure get method" + request.getSession().getId();
+
+
+    }
+}
---
+++ src/main/java/com/rkworks/SpringSecurityLearning/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.rkworks.SpringSecurityLearning.SecurityConfig;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+// BY USING THIS BEAN WE CAN CUSTOMISE THE DEFAULT BASIC AUTH METHOD AS WE NEED.
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+//        http.csrf(customizer ->customizer.disable());// TO DISABLE THE NEED FOR PASSING X-CSRF-TOKEN FOR PUT,POST AND DELETE METHODS
+//        http.authorizeHttpRequests(request->request.anyRequest().authenticated());//TO AUTHENTICATE ALL REQUEST
+//        http.httpBasic(Customizer.withDefaults());//TO ADD AUTHENTICATION IN POSTMAN AND ALSO TO REMOVE NEED FOR FROM IN WEBPAGE
+//        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//TO MAKE HTTP STATELESS
+// return http.build();
+        //written using lamda expression simplified form.
+      return http
+       .csrf(customizer ->customizer.disable())
+              .authorizeHttpRequests(request->request.anyRequest().authenticated())
+              .httpBasic(Customizer.withDefaults())
+              .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+              .build();
+
+    }
+
+    //USING AN INBUILD INTERFACE WHICH CAN TAKE CUSTOMISED USER DETAILS FROM DB OTHER THAN HARD CODING IN APP PROPERTY FILES.
+    @Bean
+    public UserDetailsService userDetailsService(){ //UserDetailsService IS AN INTERFACE WE NEED TO FIND A CLASS WHICH IMPLEMENTS IT TO GET RETURN TYPE
+   //[public class InMemoryUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService]
+        UserDetails user1 = User.withDefaultPasswordEncoder()  //THE METHID IS DEPRECIATED , SHOULD CREATE A NEW METHOD TO ENCRPYT PASSWORD.
+                .username("Ram")
+                .password("123")              //HARD CODING VALUES JUST OT SHOW EXAMPLE .
+                .roles("USER")                // SHOULD TAKE THIS VALUES FROM DB.
+                .build();
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("Sam")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2);//
+    }
+
+    
+}
